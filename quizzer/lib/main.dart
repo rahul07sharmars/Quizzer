26126a6b6c59d07fad0847d859de738c33969c44
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2021-05-16 08:27:58 +0530

    question displayed

--- quizzer/lib/main.dart
+++ quizzer/lib/main.dart
@@ -25,16 +25,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    )
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+
+  var question_no = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -47,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[question_no],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,7 +70,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                
                 setState(() {
+                  if(question_no<questions.length-1)
+                    question_no++;
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green));
                 });
               },
@@ -92,13 +94,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  if(question_no<questions.length-1)
+                    question_no++;
                   scoreKeeper.add(Icon(Icons.close, color: Colors.red));
                 });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        //Tdo: Add a Row here as your score keeper
         Row(
           children: scoreKeeper,
         )
